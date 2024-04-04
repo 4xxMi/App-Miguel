354cc819d37d0e9cc63ea1cc724ffa2e4b8f8163
Miguel <4xx136@gmail.com>
2024-04-04 13:52:39 -0300

    funfando !

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import EmojiPicker from './src/components/EmojiPicker';
 import EmojiList from './src/components/EmojiList';
 import EmojiSticker from './src/components/EmojiSticker';
 import IconButton from './src/components/IconButton';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const PlaceholderImage = require('./src/assets/images/background-image.png');
 
@@ -50,11 +51,10 @@ export default function App() {
 
   return (
     <GestureHandlerRootView style={styles.container}>
-    <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} selectedImage={selectedImage} />
-        {pickedEmoji && <EmojiSticker imageSize={40} stickerSoure={pickedEmoji} /> }
-      </View>
+        {pickedEmoji !== null ? <EmojiSticker imageSize={40} stickerSource={pickedEmoji} /> : null}
+      </View>      
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
           <View style={styles.optionsRow}>
@@ -62,21 +62,23 @@ export default function App() {
             <CircleButton onPress={onAddSticker} />
             <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
           </View>
-          </View>
+        </View>
       ) : (
         <View style={styles.footerContainer}>
-          <Button label="Escolher uma foto" theme="primary" onPress={pickImageAsync} />
-          <Button label="Usar esta foto" onPress={() => setShowAppOptions(true)} />
+          <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
+          <Button
+            label="Use this photo" onPress={() => setShowAppOptions(true)}
+          />
         </View>
       )}
       <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
     </GestureHandlerRootView>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
